7d9d3e8a9915907de02bb243f06cabd4bc288650
dragoncz <dragoncz@gmail.com>
2024-05-21 16:23:40 +0800

    refactor: re-organize plugin list

--- lua/community.lua
+++ lua/community.lua
@@ -8,18 +8,14 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
 
-  -- Editing
-  { import = "astrocommunity/editing-support/vim-move" },
-  -- Motion
-  { import = "astrocommunity/motion/vim-matchup" },
-  { import = "astrocommunity/motion/nvim-surround" },
-  -- Color scheme
   { import = "astrocommunity.colorscheme.everforest" },
-  -- Git
-  { import = "astrocommunity.git.git-blame-nvim" },
-  -- Completion
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.completion.codeium-vim" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.editing-support.vim-move" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.motion.vim-matchup" },
+  { import = "astrocommunity.motion.nvim-surround" },
+
   -- Pack
   { import = "astrocommunity.pack.rainbow-delimiter-indent-blankline" },
   { import = "astrocommunity.pack.typescript" },
