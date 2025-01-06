dcf5134860ab88214d18be30bb0790d73f59c436
dragoncz <dragoncz@gmail.com>
2025-01-06 15:39:23 +0800

    feat: use github copilot and add some community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -7,12 +7,12 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
-
   { import = "astrocommunity.color.transparent-nvim" },
-  { import = "astrocommunity.completion.codeium-vim" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.completion.copilot-cmp" },
   { import = "astrocommunity.editing-support.vim-move" },
   { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.git.mini-diff" },
   { import = "astrocommunity.motion.vim-matchup" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.syntax.vim-easy-align" },
@@ -20,6 +20,7 @@ return {
   -- Recipes
   { import = "astrocommunity.recipes.auto-session-restore" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.recipes.telescope-nvchad-theme" },
 
   -- Pack
   { import = "astrocommunity.pack.rainbow-delimiter-indent-blankline" },
